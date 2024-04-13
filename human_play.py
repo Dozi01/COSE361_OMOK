9aff419e92e59d269be61df49cccd8d9b0b1991e
Dozi01 <ytooom019@naver.com>
2024-04-13 23:56:56 +0900

    MTCS implement

--- human_play.py
+++ human_play.py
@@ -10,10 +10,6 @@ from __future__ import print_function
 import pickle
 from game import Board, Game
 from mcts_pure import MCTSPlayer as MCTS_Pure
-from mcts_alphaZero import MCTSPlayer
-from policy_value_net_numpy import PolicyValueNetNumpy
-# from policy_value_net import PolicyValueNet  # Theano and Lasagne
-from policy_value_net_pytorch import PolicyValueNet  # Pytorch
 
 
 class Human(object):
@@ -46,31 +42,15 @@ class Human(object):
 
 def run():
     n = 5
-    width, height = 6, 6
-    model_file = 'best_policy.model'
+    width, height = 9, 9
+    # model_file = 'best_policy.model'
     try:
         board = Board(width=width, height=height, n_in_row=n)
         game = Game(board)
 
         # ############### human VS AI ###################
-        # load the trained policy_value_net in either Theano/Lasagne, PyTorch or TensorFlow
 
-        best_policy = PolicyValueNet(width, height, model_file = model_file)
-        mcts_player = MCTSPlayer(best_policy.policy_value_fn, c_puct=5, n_playout=400)
-
-        # load the provided model (trained in Theano/Lasagne) into a MCTS player written in pure numpy
-        # try:
-        #     policy_param = pickle.load(open(model_file, 'rb'))
-        # except:
-        #     policy_param = pickle.load(open(model_file, 'rb'),
-        #                                encoding='bytes')  # To support python3
-        # best_policy = PolicyValueNetNumpy(width, height, policy_param)
-        # mcts_player = MCTSPlayer(best_policy.policy_value_fn,
-        #                          c_puct=5,
-        #                          n_playout=400)  # set larger n_playout for better performance
-
-        # uncomment the following line to play with pure MCTS (it's much weaker even with a larger n_playout)
-        # mcts_player = MCTS_Pure(c_puct=5, n_playout=1000)
+        mcts_player = MCTS_Pure(c_puct=5, n_playout=1000)
 
         # human player, input your move in the format: 2,3
         human = Human()
